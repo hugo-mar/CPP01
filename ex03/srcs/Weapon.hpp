01ca433275b409a6550ed1c882163f742c640479
Hugo Miguel Martins Alberto Rodrigues Dias <hugo-mar@c2r8s6.42lisboa.com>
2025-06-23 13:50:30 +0100

    ex03 downgrade - no explicit

--- ex03/srcs/Weapon.hpp
+++ ex03/srcs/Weapon.hpp
@@ -17,11 +17,12 @@
 
 class Weapon
 {
+	
 private:
 	std::string _type;
 
 public:
-	explicit Weapon( const std::string& type );
+	Weapon( const std::string& type );
 
 	const std::string&	getType() const;
 	void				setType( const std::string& type);
